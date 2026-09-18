f3a83524b43f5c3b8744d55c348853a638638d1b
LeeQianXi <qianxilee232@163.com>
2026-09-18 12:59:16 +0800

    example

--- examples/elimination/chapters/01/java/com/example/elimination/EliminationMod.java
+++
@@ -1,8 +0,0 @@
-package com.example.elimination;
-
-import net.minecraftforge.fml.common.Mod;
-
-@Mod(EliminationMod.MODID)
-public final class EliminationMod {
-    public static final String MODID = "elimination_addon";
-}
